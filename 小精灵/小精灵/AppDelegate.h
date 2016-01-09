7a473587d4d34d45348bd0d8779824e84dfde8e3
mocne <kaifeng0605@sina.com>
2016-01-09 19:05:26 +0800

    Create the project

---
+++ 小精灵/小精灵/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  小精灵
+//
+//  Created by kaifeng on 16/1/9.
+//  Copyright © 2016年 kaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 小精灵/小精灵/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  小精灵
+//
+//  Created by kaifeng on 16/1/9.
+//  Copyright © 2016年 kaifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
